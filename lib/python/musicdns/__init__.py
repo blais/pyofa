7e1b6ef23e4a48b51ac420a3833324c17c0fd30b
blais <unknown>
2007-09-02 00:57:18 +0000

    Initial import of musicdns code

---
+++ lib/python/musicdns/__init__.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+# Copyright (C) 2006 Lukáš Lalinský
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+import sys
+
+from PyQt4 import QtCore
+try:
+    from picard.musicdns import ofa
+except ImportError:
+    ofa = None
+from picard import version_string
+from picard.const import MUSICDNS_KEY
+from picard.util import encode_filename, partial
+from picard.util.thread import proxy_to_main
+
+
+class OFA(QtCore.QObject):
+
+    def __init__(self):
+        QtCore.QObject.__init__(self)
+        if not ofa:
+            self.log.warning(
+                "Libofa not found! Fingerprinting will be disabled.")
+        self._decoders = []
+        plugins = ["avcodec", "directshow", "quicktime", "gstreamer"]
+        for name in plugins:
+            try:
+                decoder = getattr(__import__("picard.musicdns." + name).musicdns, name)
+                self._decoders.append(decoder)
+            except ImportError:
+                pass
+        if not self._decoders:
+            self.log.warning(
+                "No decoders found! Fingerprinting will be disabled.")
+
+    def init(self):
+        for decoder in self._decoders:
+            decoder.init()
+
+    def done(self):
+        for decoder in self._decoders:
+            decoder.done()
+
+    def create_fingerprint(self, filename):
+        """Decode the specified file and calculate a fingerprint."""
+        print >> sys.stderr, 'create_fingerprint:', filename
+        if ofa is None:
+            return None, 0
+        filename = encode_filename(filename)
+        for decoder in self._decoders:
+            self.log.debug("Decoding using %r...", decoder.__name__)
+            try:
+                result = decoder.decode(filename)
+            except Exception:
+                continue
+            self.log.debug("Fingerprinting...")
+            if result:
+                buffer, samples, sample_rate, stereo, duration = result
+                fingerprint = ofa.create_print(buffer, samples, sample_rate, stereo)
+                return fingerprint, duration
+        return None, 0
+
+    def _lookup_finished(self, handler, file, document, http, error):
+        try:
+            puid = document.metadata[0].track[0].puid_list[0].puid[0].id
+        except (AttributeError, IndexError):
+            puid = None
+        # for some reason MusicDNS started to return these bogus PUIDs
+        if puid == '00000000-0000-0000-0000-000000000000':
+            puid = None
+        handler(file, puid)
+
+    def _lookup_fingerprint(self, file, fingerprint, handler, length=0):
+        if file.state != file.PENDING:
+            handler(file, None)
+            return
+        self.tagger.window.set_statusbar_message(N_("Looking up the fingerprint for file %s..."), file.filename)
+        self.tagger.xmlws.query_musicdns(partial(self._lookup_finished, handler, file),
+            rmt='0',
+            lkt='1',
+            cid=MUSICDNS_KEY,
+            cvr="MusicBrainz Picard-%s" % version_string,
+            fpt=fingerprint,
+            dur=str(file.metadata.length or length),
+            brt=str(file.metadata.get("~#bitrate", 0)),
+            fmt=file.metadata["~format"],
+            art=file.metadata["artist"],
+            ttl=file.metadata["title"],
+            alb=file.metadata["album"],
+            tnm=file.metadata["tracknumber"],
+            gnr=file.metadata["genre"],
+            yrr=file.metadata["date"][:4])
+
+    def _create_fingerprint(self, file, handler):
+        if file.state != file.PENDING:
+            handler(file, None)
+            return
+        self.tagger.window.set_statusbar_message(N_("Creating fingerprint for file %s..."), file.filename)
+        filename = encode_filename(file.filename)
+        fingerprint = None
+        for decoder in self._decoders:
+            self.log.debug("Decoding using %r...", decoder.__name__)
+            try:
+                result = decoder.decode(filename)
+            except Exception:
+                continue
+            if result:
+                self.log.debug("Fingerprinting...")
+                buffer, samples, sample_rate, stereo, duration = result
+                fingerprint = ofa.create_print(buffer, samples, sample_rate, stereo)
+                if fingerprint:
+                    proxy_to_main(self._lookup_fingerprint, file, fingerprint, handler, duration)
+                    return
+                else:
+                    break
+        proxy_to_main(handler, file, None)
+
+    def analyze(self, file, handler):
+        print >> sys.stderr, 'analyze:', file
+
+        if 'musicip_puid' in file.metadata:
+            handler(file, file.metadata.getall('musicip_puid')[0])
+        else:
+            if 'musicip_fingerprint' in file.metadata:
+                fingerprint = file.metadata.getall('musicip_fingerprint')[0]
+                self._lookup_fingerprint(file, fingerprint, handler)
+            elif ofa is not None:
+                self.tagger.analyze_thread.add_task(self._create_fingerprint, file, handler)
+            else:
+                handler(file, None)
---
+++ lib/python/musicdns/avcodec.c
@@ -0,0 +1,333 @@
+/*
+ * Picard, the next-generation MusicBrainz tagger
+ * Copyright (C) 2006 Lukáš Lalinský
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* avcodec/avformat-based audio decoder for Picard */
+
+#ifdef _MSC_VER
+#define INT64_C(val) val##i64
+#define inline __inline
+#endif
+
+#include <avcodec.h>
+#include <avformat.h>
+#include <Python.h>
+
+#ifdef _WIN32
+
+#include <string.h>
+#include <avio.h>
+#include <fcntl.h>
+#include <windows.h>
+
+static int
+ufile_open(URLContext *h, const char *filename, int flags)
+{
+    int access;
+    int fd;
+    int size;
+    const char *ptr;
+    wchar_t *w_filename, *w_ptr;
+    char *ansi_filename;
+
+    /* skip "ufile:" */
+    filename += 6;
+    ptr = filename;
+    w_filename = malloc(strlen(filename));
+    w_ptr = w_filename;
+    while (*ptr)
+    {
+        char a = (*ptr++) - 0x20;
+        char b = (*ptr++) - 0x20;
+        char c = (*ptr++) - 0x20;
+        char d = (*ptr++) - 0x20;
+    	  *w_ptr = a | (b << 4) | (c << 8) | (d << 12);
+    	  if (*w_ptr == 0)
+					break;
+				w_ptr++;				   
+		}
+		*w_ptr = 0;
+
+    if (flags & URL_RDWR) {
+        access = O_CREAT | O_TRUNC | O_RDWR;
+    } else if (flags & URL_WRONLY) {
+        access = O_CREAT | O_TRUNC | O_WRONLY;
+    } else {
+        access = O_RDONLY;
+    }
+    access |= O_BINARY;
+
+    if (GetVersion() < 0x80000000) {
+        fd = _wopen(w_filename, access, 0666);
+    }
+    else {
+        fd = -1;
+        size = wcslen(w_filename) + 2;
+        ansi_filename = malloc(size);
+        if (ansi_filename) { 
+            if (WideCharToMultiByte(CP_ACP, 0, w_filename, -1, ansi_filename, size, NULL, NULL) > 0) {
+	  	          fd = open(ansi_filename, access, 0666);
+						}
+            free(ansi_filename);
+				}
+    }
+
+    free(w_filename);
+
+    if (fd < 0)
+        return AVERROR(ENOENT);
+    h->priv_data = (void *)(size_t)fd;
+    return 0;
+}
+
+static int
+ufile_read(URLContext *h, unsigned char *buf, int size)
+{
+    int fd = (size_t)h->priv_data;
+    return read(fd, buf, size);
+}
+
+static int
+ufile_write(URLContext *h, unsigned char *buf, int size)
+{
+    int fd = (size_t)h->priv_data;
+    return write(fd, buf, size);
+}
+
+static offset_t
+ufile_seek(URLContext *h, offset_t pos, int whence)
+{
+    int fd = (size_t)h->priv_data;
+    return lseek(fd, pos, whence);
+}
+
+static int
+ufile_close(URLContext *h)
+{
+    int fd = (size_t)h->priv_data;
+    return _close(fd);
+}
+
+URLProtocol ufile_protocol = {
+    "ufile",
+    ufile_open,
+    ufile_read,
+    ufile_write,
+    ufile_seek,
+    ufile_close,
+};
+
+#endif
+
+static PyObject *
+init(PyObject *self, PyObject *args)
+{
+    av_register_all();
+#ifdef _WIN32
+    register_protocol(&ufile_protocol);
+#endif
+    Py_RETURN_NONE;
+}
+
+static PyObject *
+done(PyObject *self, PyObject *args)
+{
+    Py_RETURN_NONE;
+}
+
+static PyObject *
+decode(PyObject *self, PyObject *args)
+{
+    AVFormatContext *format_context;
+    AVCodecContext *codec_context;
+    AVCodec *codec;
+    PyObject *filename;
+    AVPacket packet;
+    unsigned int i;
+    int buffer_size, channels, sample_rate, size, len, output_size;
+    uint8_t *buffer, *buffer_ptr, *data;
+    PyThreadState *_save;
+    int err;
+
+#ifdef _WIN32
+    Py_ssize_t w_length;
+    wchar_t *w_filename, *w_ptr;
+    char *e_filename, *e_ptr;
+
+    if (!PyArg_ParseTuple(args, "U", &filename))
+        return NULL;
+
+    /* get the original filename as wchar_t* */
+    w_length = PyUnicode_GetSize(filename) + 1;
+		w_filename = malloc(w_length * sizeof(wchar_t));
+		if (!w_filename)
+        return NULL;
+		memset(w_filename, 0, w_length * sizeof(wchar_t));
+		PyUnicode_AsWideChar((PyUnicodeObject *)filename, w_filename, w_length - 1);
+
+    /* 'encode' the filename, so we can pass it as char* */
+		e_filename = malloc(w_length * sizeof(wchar_t) * 2 + w_length + 7);
+		if (!e_filename)
+        return NULL;
+		strcpy(e_filename, "ufile:");
+		w_ptr = w_filename;
+		e_ptr = e_filename + 6;
+		while (*w_ptr) {
+		    *e_ptr++ = 0x20 + ((*w_ptr >>  0) & 0x0F);
+		    *e_ptr++ = 0x20 + ((*w_ptr >>  4) & 0x0F);
+		    *e_ptr++ = 0x20 + ((*w_ptr >>  8) & 0x0F);
+		    *e_ptr++ = 0x20 + ((*w_ptr >> 12) & 0x0F);
+		    w_ptr++;
+		}
+		*e_ptr++ = 0x20;
+		*e_ptr++ = 0x20;
+		*e_ptr++ = 0x20;
+		*e_ptr++ = 0x20;
+		/* copy ASCII filename to the end for extension-based format detection */		
+		w_ptr = w_filename;
+		while (*w_ptr) {
+		    *e_ptr++ = (*w_ptr++) & 0xFF;
+		}
+		*e_ptr = 0;
+		
+    Py_UNBLOCK_THREADS
+    err = av_open_input_file(&format_context, e_filename, NULL, 0, NULL)
+    if (err != 0) {
+        Py_BLOCK_THREADS
+        free(e_filename);
+        free(w_filename);
+        PyErr_SetString(PyExc_Exception, "Couldn't open the file: %s." % err);
+        return NULL;
+    }
+
+    free(e_filename);
+    free(w_filename);
+#else
+    if (!PyArg_ParseTuple(args, "S", &filename))
+        return NULL;
+
+    Py_UNBLOCK_THREADS
+    err = av_open_input_file(&format_context, PyString_AS_STRING(filename), NULL, 0, NULL);
+    if (err != 0) {
+        Py_BLOCK_THREADS
+        fprintf(stderr, "Error: %d", err);
+        PyErr_SetString(PyExc_Exception, "Couldn't open the file.");
+        return NULL;
+    }
+#endif
+
+    if (av_find_stream_info(format_context) < 0) {
+        Py_BLOCK_THREADS
+        PyErr_SetString(PyExc_Exception, "Couldn't find stream information in the file.");
+        return NULL;
+    }
+
+#ifndef NDEBUG
+    dump_format(format_context, 0, PyString_AS_STRING(filename), 0);
+#endif
+
+    codec_context = NULL;
+    for (i = 0; i < format_context->nb_streams; i++) {
+        codec_context = (AVCodecContext *)format_context->streams[i]->codec;
+        if (codec_context && codec_context->codec_type == CODEC_TYPE_AUDIO)
+            break;
+    }
+    if (codec_context == NULL) {
+        Py_BLOCK_THREADS
+        PyErr_SetString(PyExc_Exception, "Couldn't find any audio stream in the file.");
+        return NULL;
+    }
+
+    codec = avcodec_find_decoder(codec_context->codec_id);
+    if (codec == NULL) {
+        Py_BLOCK_THREADS
+        PyErr_SetString(PyExc_Exception, "Unknown codec.");
+        return NULL;
+    }
+
+    if (avcodec_open(codec_context, codec) < 0) {
+        Py_BLOCK_THREADS
+        PyErr_SetString(PyExc_Exception, "Couldn't open the codec.");
+        return NULL;
+    }
+
+    channels = codec_context->channels;
+    sample_rate = codec_context->sample_rate;
+
+    buffer_size = 135 * channels * sample_rate * 2;
+    buffer = (uint8_t *)malloc(buffer_size + AVCODEC_MAX_AUDIO_FRAME_SIZE);
+    buffer_ptr = buffer;
+    memset(buffer, 0, buffer_size);
+
+    while (buffer_size > 0) {
+        if (av_read_frame(format_context, &packet) < 0)
+            break;
+
+        size = packet.size;
+        data = packet.data;
+
+        while (size > 0) {
+            len = avcodec_decode_audio(codec_context, (int16_t *)buffer_ptr, &output_size, data, size);
+
+            if (len < 0)
+                break;
+
+            size -= len;
+            data += len;
+
+            if (output_size <= 0)
+                continue;
+
+            buffer_ptr += output_size;
+            buffer_size -= output_size;
+            if (buffer_size <= 0)
+                break;
+        }
+
+        if (packet.data)
+            av_free_packet(&packet);
+    }
+
+    if (codec_context)
+        avcodec_close(codec_context);
+
+    if (format_context)
+        av_close_input_file(format_context);
+
+    Py_BLOCK_THREADS
+
+    return Py_BuildValue("(N,i,i,i,i)",
+        PyCObject_FromVoidPtr(buffer, free),
+        (buffer_ptr - buffer) / 2,
+        sample_rate,
+        channels == 2 ? 1 : 0,
+        0);
+}
+
+static PyMethodDef avcodec_methods[] = {
+    {"init", init, METH_VARARGS, ""},
+    {"done", done, METH_VARARGS, ""},
+    {"decode", decode, METH_VARARGS, ""},
+    {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC
+initavcodec(void)
+{
+    (void)Py_InitModule("avcodec", avcodec_methods);
+}
